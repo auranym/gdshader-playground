b20598d451450a7360495454a380861d70271e96
auranym <auranym.media@gmail.com>
2024-08-31 20:51:00 -0700

    set up build command; set up communicating data between godot app and shell page

--- src/app_shell/app-shell.js
+++ src/app_shell/app-shell.js
@@ -1,1 +1,4 @@
-ENGINE.startGame();
+window.ENGINE.startGame();
+/** Used to send code to Godot app. */
+window.shaderCode = '';
+window.updateShell = () => document.querySelector('p').innerHTML = window.shaderCode;
--- src/godot_app/main.gd
+++ src/godot_app/main.gd
@@ -1,15 +1,14 @@
 extends Control
 
 @onready var _label: Label = $SubViewportContainer/SubViewport/Label
-var _update_callback = JavaScriptBridge.create_callback(_update)
+var _update_ref = JavaScriptBridge.create_callback(_update)
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var window = JavaScriptBridge.get_interface("window")
-	window.godotAppUpdate = _update_callback
+	window.updateApp = _update_ref
 
 
-func _update(args):
-	if args and args[0] is String:
-		_label.text = args[0]
+func _update(_args):
+	var window = JavaScriptBridge.get_interface("window")
+	_label.text = window.shaderCode
