4cdd5ffdd18a30e813dd3652c287731c2ca9ab9c
auranym <auranym.media@gmail.com>
2024-09-03 16:41:14 -0700

    fleshed out page and style to mirror godot's web style, added ace-editor web component and update button

--- src/app_shell/app-shell.js
+++ src/app_shell/app-shell.js
@@ -1,3 +1,8 @@
+// Detect if not mobile, and if so, add theme class to body
+if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+  document.querySelector('body').classList.add('desktop');
+}
+
 window.ENGINE.startGame();
 /** Used to send code to Godot app. */
 window.shaderCode = '';
